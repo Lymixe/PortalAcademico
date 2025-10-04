81ce980c64d36092ee54151fcddff51e8edb7342
Lymixe <peraltacenepoemily@gmail.com>
2025-10-04 01:59:35 -0500

    Models

--- Program.cs
+++ Program.cs
@@ -5,6 +5,7 @@ using PortalAcademico.Data;
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
+
 var connectionString = builder.Configuration.GetConnectionString("DefaultConnection") ?? throw new InvalidOperationException("Connection string 'DefaultConnection' not found.");
 builder.Services.AddDbContext<ApplicationDbContext>(options =>
     options.UseSqlite(connectionString));
